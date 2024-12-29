8dae05c86f51f42f1881e6b1e4793a5d5a77486a
rf0x3d <yarekfrolov@gmail.com>
2024-12-29 23:07:32 +0400

    fix: change gateway url
    Signed-off-by: rf0x3d <yarekfrolov@gmail.com>

--- hackem_yandex_dialogs/utils/hackem_api.py
+++ hackem_yandex_dialogs/utils/hackem_api.py
@@ -4,7 +4,7 @@ from hackem_yandex_dialogs.utils import make_request
 
 async def make_api_request(method: str, post: bool = True, **data):
     return await make_request(
-        f"https://gateway.hackerembassy.site/{method}",
+        f"https://gateway.hackem.cc/{method}",
         "post" if post else "get",
         headers={"User-Agent": f"rf0x3d/1.0.0 ({PROJECT_NAME})"},
         json={"token": config.get_item("app", "api_token"), **data},
